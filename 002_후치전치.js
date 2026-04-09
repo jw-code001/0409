f57c16ab348086401d50827a1961bdfe6be28800
jw-code001 <jwgame0804@gmail.com>
2026-04-09 20:02:49 +0900

    수업01

---
+++ 002_후치전치.js
@@ -0,0 +1,13 @@
+let count = 100;
+count = 10;
+// let count = 10; --> 두번 let 안됨
+
+count = count +10; // ---> count += 10;과 동일 --> 20
+count -= 2; // --> 18
+count++; // --> 18
+++count; // --> 20
+
+console.log(count) // --> 20
+console.log(count++) // --> 20
+console.log(++count) // --> 22
+
